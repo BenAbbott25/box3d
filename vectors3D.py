219d5199a9e4c719926ebb89d7b31061eccf365a
BenAbbott25 <benabbott250@gmail.com>
2024-05-17 03:14:13 +0100

    A Box!!

--- vectors3D.py
+++ vectors3D.py
@@ -2,14 +2,47 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-vector1 = {'coords': np.array([1, 1, 1]), 'color': 'blue'}
+vector1 = {'coords': np.array([1, 2, 3]), 'color': 'blue'}
 
-def draw_vector(vector, color):
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.quiver(0, 0, 0, vector['coords'][0], vector['coords'][1], vector['coords'][2], color=color)
+fig = plt.figure()
 
-draw_vector(vector1, 'blue')
+ax = fig.add_subplot(111, projection='3d', aspect='equal')
+
+def draw_vector(vector, ax):
+    ax.quiver(0, 0, 0, vector['coords'][0], vector['coords'][1], vector['coords'][2], color=vector['color'])
+
+def draw_box(vector, ax):
+    vertices = [
+        [[0, 0, 0]],
+        
+        [[vector['coords'][0], 0, 0],
+        [0, vector['coords'][1], 0],
+        [0, 0, vector['coords'][2]],],
+
+        [[vector['coords'][0], vector['coords'][1], 0],
+        [vector['coords'][0], 0, vector['coords'][2]],
+        [0, vector['coords'][1], vector['coords'][2]],],
+
+        [[vector['coords'][0], vector['coords'][1], vector['coords'][2]]]
+    ]
+
+    for i in range(len(vertices)):
+        print(i)
+        print(vertices[i])
+        if i == 0:
+            continue
+        else:
+            set1 = vertices[i-1]
+            set2 = vertices[i]
+            print(set1)
+            print(set2)
+            for s1 in set1:
+                for s2 in set2:
+                    if ((s1[0] != s2[0]) or (s1[1] != s2[1]) or (s1[2] != s2[2])) and ((s1[0] == s2[0]) or (s1[1] == s2[1]) and (s1[2] == s2[2])):
+                        ax.quiver(s1[0], s1[1], s1[2], s2[0] - s1[0], s2[1] - s1[1], s2[2] - s1[2], color='red', arrow_length_ratio=0)
+
+draw_vector(vector1, ax)
+draw_box(vector1, ax)
 
 plt.show()
 
